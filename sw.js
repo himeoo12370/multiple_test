0f88110057e9751e9b9c898133a73e3e1c16cc0e
himeoo12370 <himeoo12370@gmail.com>
2026-05-29 00:31:10 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,3 @@
-// sw.js
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-});
-
-// 必須要監聽 fetch 事件，PWA 才會被判定為可安裝
 self.addEventListener('fetch', (e) => {
-  // 目前不做任何快取攔截，直接放行網路請求
+  // 放行所有請求，這是觸發 PWA 安裝的最低要求
 });
